73485ed6d94898a323e83fc627f278540324358c
KemosahbeAndres <andrestj1996@gmail.com>
2018-06-04 15:20:07 -0400

    Multicast Funcionando

--- app/src/main/java/cl/proyecto/kemosahbe/lanintercom/MainActivity.java
+++ app/src/main/java/cl/proyecto/kemosahbe/lanintercom/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -18,13 +21,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
 import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.StandardProtocolFamily;
+import java.net.StandardSocketOptions;
+import java.nio.channels.DatagramChannel;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
 
@@ -34,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
     Boolean iniciado = false;
     final String tag = "MAIN";
     private Messenger messenger, sMessenger;
+    WifiManager wm;
+    WifiManager.MulticastLock ml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +61,20 @@ public class MainActivity extends AppCompatActivity {
         msg = (EditText) findViewById(R.id.msg);
         txt.setText("Servicio No iniciado");
         //Comprobar que el dispositivo este conectado a una red WIFI
+        //Admitir Multicast.
+        wm = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        ml = wm.createMulticastLock("MultiCast");
+        ml.acquire();
+        WifiInfo wi = wm.getConnectionInfo();
+        if(wi != null){
+            Log.i(tag, "get BSSID: "+wi.getBSSID()+" MAC: "+wi.getMacAddress()+" IPV4: "+wi.getIpAddress());
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        messenger = new Messenger(new mHandler());
+        //messenger = new Messenger(new mHandler());
     }
 
     public void iniciar(View v){
@@ -78,9 +103,188 @@ public class MainActivity extends AppCompatActivity {
 
     public void enviar(View v){
         String mensaje = msg.getText().toString();
+        Log.i(tag,"Mensaje: "+mensaje);
+        mAsyncTask mTask = new mAsyncTask(mensaje);
+        mTask.execute();
+    }
+
+    class mAsyncTask extends AsyncTask<Void,Boolean,Integer>{
+        private MulticastSocket socket;
+        private DatagramChannel channel;
+        private String buffer;
+        private InetSocketAddress serverAddress = new InetSocketAddress("239.255.255.249",1800);
+        private DatagramPacket packet;
+        //vbkhb
+        NetworkInterface mInterface;
+        Enumeration<NetworkInterface> mInterfaces;
+        Enumeration<InetAddress> mAddress;
+        int ifaceCount;
+
+        public mAsyncTask(String msg) {
+            this.buffer = msg;
+        }
+
+        public void println(String msg){
+            Log.i(tag, msg);
+        }
+
+        public void initInfo(){
+            try{
+                mInterfaces = NetworkInterface.getNetworkInterfaces();
+                ifaceCount = Collections.list(mInterfaces).size();
+                Log.i(tag,"N° Interfaces: "+ifaceCount+"\r\n");
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+            for(int i=0;i<=ifaceCount-1;i++) {
+                showInfo(i);
+            }
+        }
+        public void showInfo(int index) {
+            try {
+                mInterfaces = NetworkInterface.getNetworkInterfaces();
+                mInterface = Collections.list(mInterfaces).get(index);
+
+                println("N°  : " + index + 1);
+                println("Name: " + mInterface.getDisplayName());
+                println("ID  : " + mInterface.getName());
+                println("MAC : " + Arrays.toString(mInterface.getHardwareAddress()));
+
+                mAddress = mInterface.getInetAddresses();
+                for (InetAddress i : Collections.list(mAddress)) {
+                    println("IP  : " + i.getHostAddress());
+                    println("HOST: " + i.getHostName());
+                }
+
+                String status = "DOWN!";
+                if (mInterface.isUp()) {
+                    status = "UP!";
+                }
+                println("STATUS: " + status);
+
+                String point = "NO!";
+                if (mInterface.isPointToPoint()) {
+                    point = "YES!";
+                }
+                println("POINT TO POINT: " + point);
+
+                String multicast = "NO!";
+                if (mInterface.supportsMulticast()) {
+                    multicast = "YES!";
+                }
+                println("MULTICAST: " + multicast);
+
+                String loopback = "NO!";
+                if (mInterface.isLoopback()) {
+                    loopback = "YES!";
+                }
+                println("LOOPBACK: " + loopback);
+
+                String virtual = "NO!";
+                if (mInterface.isVirtual()) {
+                    virtual = "YES!";
+                }
+                println("VIRTUAL: " + virtual);
+
+                //System.in.read();
+                println("");
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        //@Override
+        //protected void onPreExecute() {
+           // initInfo();
+        //}
+
+        @Override
+        protected void onProgressUpdate(Boolean... values) {
+            //Toast.makeText(MainActivity.this, "Mensaje enviado.", Toast.LENGTH_SHORT).show();
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected Integer doInBackground(Void... voids) {
+            //initInfo();
+            //InetAddress groupAddr = null;
+
+            try {
+                NetworkInterface Interf = NetworkInterface.getByName("wlan0");
+                MulticastSocket socket  = new MulticastSocket();
+                socket.setReuseAddress(true);
+                Log.i(tag,"Multicast Sender running at: "+socket.getLocalSocketAddress());
+                socket.setNetworkInterface(Interf);
+                InetAddress addr = InetAddress.getByName("239.255.255.249");
+                socket.joinGroup(addr);
+
+                byte[] data = buffer.getBytes();
+                DatagramPacket dp = new DatagramPacket(data,data.length, addr, 1800);
+
+                socket.send(dp);
+
+                socket.leaveGroup(addr);
+                socket.close();
+
+                //Enumeration<InetAddress> mAddress = Interf.getInetAddresses();
+                //for(InetAddress i : Collections.list(mAddress)){
+                    //println("IP  : "+i.getHostAddress());
+                    //println("HOST: "+i.getHostName());
+                    //try {
+                        //if (InetAddress.getByName(i.getHostAddress()) instanceof Inet4Address) {
+                            //groupAddr = InetAddress.getByName(i.getHostAddress());
+                            //break;
+                        //}
+                    //}catch(Exception e){
+                        //e.printStackTrace();
+                    //}
+                //}
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //openChannel(serverAddress, groupAddr);
+
+            return 0;
+        }
+        private void openSocket(){
+            try {
+                socket = new MulticastSocket(1800);
+                socket.setReuseAddress(true);
+                packet = new DatagramPacket(buffer.getBytes(), buffer.length(),InetAddress.getByName("192.168.0.16"),1800);
+                socket.joinGroup(serverAddress.getAddress());
+                socket.send(packet);
+                Log.i(tag,"Paquete enviado: "+buffer+ " "+packet.getAddress()+ " "+packet.getPort());
+                //this.onProgressUpdate();
+                socket.leaveGroup(serverAddress.getAddress());
+                socket.close();
+                socket = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        private void openChannel(InetSocketAddress address, InetAddress group){
+            openChannel(address.getAddress(),group,address.getPort());
+        }
+        private void openChannel(InetAddress address, InetAddress group, int port){
+            try {
+                channel = DatagramChannel.open();
+                channel.socket().setReuseAddress(true);
+                channel.socket().setBroadcast(true);
+                //channel.setOption(StandardSocketOptions.IP_MULTICAST_TTL, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void enviarObsolete(View v){
+        String mensaje = msg.getText().toString();
         if (mensaje != "") {
             Bundle mBundle = new Bundle();
             mBundle.putString("mensaje", mensaje);
+            Log.i(tag,"Mensaje: "+mensaje);
             Message mMessage = Message.obtain();
             mMessage.setData(mBundle);
             try {
@@ -108,7 +312,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mConnection != null) unbindService(mConnection);
+        ml.release();
+        ml = null;
+        //if(mConnection != null) unbindService(mConnection);
     }
 
     ServiceConnection mConnection = new ServiceConnection() {
--- app/src/main/java/cl/proyecto/kemosahbe/lanintercom/ServicioMensajeria.java
+++ app/src/main/java/cl/proyecto/kemosahbe/lanintercom/ServicioMensajeria.java
@@ -1,7 +1,9 @@
 package cl.proyecto.kemosahbe.lanintercom;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,17 +13,24 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.SocketImpl;
+import java.net.SocketImplFactory;
+import java.net.SocketOptions;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -33,9 +42,10 @@ public class ServicioMensajeria extends Service {
     private static final String tag = "MENSAJERIA";
     private String usuario, destinatario;
     private InetAddress localAddress, serverAddress;
+    private InetSocketAddress sockaddr;
     private int serverPort;
-    private Socket socket = null;
-    //private OutputStream os;
+    private MulticastSocket socket = null;
+    private DatagramPacket outPackage, inPackage;
     private DataOutputStream output = null;
 
     @Override
@@ -75,14 +85,17 @@ public class ServicioMensajeria extends Service {
     public boolean onUnbind(Intent intent) {
         messenger = null;
         uMessenger = null;
-        if(!socket.isClosed()){
-            try {
-                socket.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+        if(socket != null) {
+            if (!socket.isClosed()) {
+                try {
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+        }else {
+            socket = null;
         }
-        socket = null;
         return super.onUnbind(intent);
     }
 
@@ -95,6 +108,7 @@ public class ServicioMensajeria extends Service {
                 serverAddress = InetAddress.getByName(extras.getString("serverAddress"));
                 Log.i(tag, extras.getString("serverAddress"));
                 serverPort = extras.getInt("serverPort");
+                sockaddr = new InetSocketAddress("239.255.255.249",1800);
                 Log.i(tag, ""+extras.getInt("serverPort"));
             } catch (Exception e) {
                 e.printStackTrace();
@@ -110,17 +124,47 @@ public class ServicioMensajeria extends Service {
 
         @Override
         public void handleMessage(Message msg) {
-            if(socket == null && output == null){
+            if(socket == null){
                 try {
-                    socket = new Socket(serverAddress, serverPort);
-                    output = new DataOutputStream(socket.getOutputStream());
+                    socket = new MulticastSocket(1800);
+                    socket.setReuseAddress(true);
+                    socket.setBroadcast(true);
                 }catch(Exception e){
                     e.printStackTrace();
+                    Toast.makeText(ServicioMensajeria.this, "Error al conectar Socket", Toast.LENGTH_SHORT).show();
+                }
+            }else if(socket.isClosed() && socket != null){
+                try{
+                    socket = new MulticastSocket(1800);
+                    socket.setReuseAddress(true);
+                    socket.setBroadcast(true);
+                }catch(Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(ServicioMensajeria.this, "Error al conectar Socket", Toast.LENGTH_SHORT).show();
                 }
             }
+            Toast.makeText(ServicioMensajeria.this, "Conectado al servidor.", Toast.LENGTH_SHORT).show();
             Bundle data = msg.getData();
             try{
-                output.writeBytes(data.getString("mensaje"));
+                byte[] msgBytes;
+                msgBytes = data.getString("mensaje").getBytes();
+                Log.i(tag,data.getString("mensaje"));
+                outPackage = new DatagramPacket(msgBytes, msgBytes.length, sockaddr);
+                socket.joinGroup(sockaddr.getAddress());
+                //socket.connect(new InetSocketAddress(serverAddress,serverPort));
+                socket.send(outPackage);
+                byte[] recvBuffer = new byte[256];
+                inPackage = new DatagramPacket(recvBuffer,recvBuffer.length);
+                //Log.i(tag,"LocalAddress: "+socket.getLocalAddress().getHostAddress());
+                socket.receive(inPackage);
+                //DatagramSocket sock = new DatagramSocket(1810);
+                //sock.receive(inPackage);
+                //String Msg = new String(inPackage.getData()).trim();
+                //Log.i(tag,"Mensaje: "+ Msg + " From: "+inPackage.getAddress().getHostAddress());
+                socket.leaveGroup(sockaddr.getAddress());
+                socket.close();
+                //sock.close();
+                Toast.makeText(ServicioMensajeria.this, "Mensaje enviado con exito.", Toast.LENGTH_SHORT).show();
             }catch(Exception e){
                 e.printStackTrace();
             }
